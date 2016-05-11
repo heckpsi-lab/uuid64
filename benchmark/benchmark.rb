8106e40f74d1f6c63200e4849935ed7ca5f6a9ba
Delton Ding <dsh0416@gmail.com>
2016-05-11 15:36:46 +0800

    Clean up code, update to 0.0.6

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,14 +1,10 @@
 require 'benchmark'
-require './uuid64_deprecated.rb'
 require File.expand_path '../lib/uuid64.rb'
 
 N = 1_000_000 # + 16_777_215
 
 Benchmark.bm do |x|
   x.report('n uuid64') { for i in 1..N; SecureRandom.uuid64 end }
-  x.report('d uuid64') { for i in 1..N; SecureRandomDeprecated.uuid64 end }
   x.report('n mongo_id') { for i in 1..N; SecureRandom.mongo_id end }
-  x.report('d mongo_id') { for i in 1..N; SecureRandomDeprecated.mongo_id end }
   x.report('n mongo_id64') { for i in 1..N; SecureRandom.mongo_id64 end }
-  x.report('d mongo_id64') { for i in 1..N; SecureRandomDeprecated.mongo_id64 end }
 end
